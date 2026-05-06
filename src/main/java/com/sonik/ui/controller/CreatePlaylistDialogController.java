061f0a54116028d540e97d9de0c8516e2a6bdeeb
jormg34587 <jorge.martin139@educa.madrid.org>
2026-05-07 00:08:58 +0200

    Changed CreatePlaylistDialogController to check for duplicates and validate fields

--- src/main/java/com/sonik/ui/controller/CreatePlaylistDialogController.java
+++ src/main/java/com/sonik/ui/controller/CreatePlaylistDialogController.java
@@ -1,48 +1,67 @@
 package com.sonik.ui.controller;
 
+import com.sonik.config.AppContext;
 import com.sonik.config.UserSession;
+import com.sonik.domain.exceptions.DataAccessException;
+import com.sonik.domain.exceptions.DuplicateIdException;
 import com.sonik.domain.exceptions.IncorrectArgumentException;
 import com.sonik.domain.model.Playlist;
 import javafx.fxml.FXML;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 public class CreatePlaylistDialogController {
 
     @FXML
+    public Label playlistNameErrorLabel;
+    @FXML
+    public Label playlistDescriptionErrorLabel;
+    @FXML
     private TextField nameTextField;
-
     @FXML
     private TextField descriptionTextField;
 
-    private Dialog<?> dialog;
-
-    public void setDialog(Dialog<?> dialog) {
-        this.dialog = dialog;
+    public void nameTextFieldOnKP(KeyEvent keyEvent) {
+        AuxiliaryMethods.setErrorLabelState(false, playlistNameErrorLabel, nameTextField, Optional.empty());
     }
 
-    public Playlist buildPlaylist() throws IncorrectArgumentException {
-        return new Playlist(
+    public void descriptionTextFieldOnKP(KeyEvent keyEvent) {
+        AuxiliaryMethods.setErrorLabelState(false, playlistDescriptionErrorLabel, descriptionTextField, Optional.empty());
+    }
+
+    public void buildPlaylist() throws IncorrectArgumentException, DuplicateIdException, DataAccessException {
+        AppContext.getPlaylistService().createPlaylist(new Playlist(
                 UserSession.getUser(),
                 nameTextField.getText(),
-                descriptionTextField.getText(), LocalDate.now()
-
-        );
-    }
-
-    public void onSaveChangesMC(MouseEvent mouseEvent) {
+                descriptionTextField.getText(), LocalDate.now()));
     }
 
     public void cancelBtnMC(MouseEvent mouseEvent) {
-        //dialog.setResult(ButtonType.CANCEL);
-        dialog.close();
+        ((Stage)playlistDescriptionErrorLabel.getScene().getWindow()).close();
     }
 
-    public void createBtnMC(MouseEvent mouseEvent) throws IncorrectArgumentException {
-        buildPlaylist();
+    public void createBtnMC(MouseEvent mouseEvent) {
+        try {
+            buildPlaylist();
+            ((Stage)playlistDescriptionErrorLabel.getScene().getWindow()).close();
+        } catch (IncorrectArgumentException e) {
+            switch (e.getErrorType()) {
+                case NULL_OBJECT_RECEIVED, INVALID_DATE -> AuxiliaryMethods.showAlert(e.getMessage());
+                case EMPTY_PLAYLIST_NAME -> AuxiliaryMethods.setErrorLabelState(
+                        true, playlistNameErrorLabel, nameTextField, Optional.of(e.getMessage()));
+                case EMPTY_PLAYLIST_DESCRIPTION -> AuxiliaryMethods.setErrorLabelState(
+                        true, playlistDescriptionErrorLabel, descriptionTextField, Optional.of(e.getMessage()));
+            }
+        } catch (DuplicateIdException e) {
+            AuxiliaryMethods.setErrorLabelState(true, playlistNameErrorLabel, nameTextField, Optional.of(e.getMessage()));
+        } catch (DataAccessException e) {
+            AuxiliaryMethods.showAlert(e.getMessage());
+        }
     }
 }

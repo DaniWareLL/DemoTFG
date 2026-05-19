3dc1e60bfed291074ac504a30e13b04091132799
jormg34587 <jorge.martin139@educa.madrid.org>
2026-05-19 12:24:10 +0200

    docs: Add javadoc to persistence/AuxiliaryMethods

--- src/main/java/com/sonik/infrastructure/persistence/AuxiliaryMethods.java
+++ src/main/java/com/sonik/infrastructure/persistence/AuxiliaryMethods.java
@@ -6,6 +6,15 @@ import jakarta.persistence.PersistenceException;
 
 public class AuxiliaryMethods {
 
+    /**
+     * Attempts to roll back the given transaction and throws a {@link DataAccessException} regardless of outcome.
+     * @param ex The exception that triggered the rollback
+     * @param tx The transaction to roll back
+     * @throws DataAccessException <ul>
+     *     <li>{@link DataAccessException#REVERT_ERROR} If the rollback itself fails</li>
+     *     <li>{@link DataAccessException#CHANGES_REVERTED} If the rollback succeeds</li>
+     * </ul>
+     */
     protected static void handleRollbackAndThrow(RuntimeException ex, EntityTransaction tx) throws DataAccessException {
         try {
             tx.rollback();
